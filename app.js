ef831deb42d1d2792f647534bdf1ce00d6694e47
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-01-19 03:48:09 +0600

    add watcher

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const fs = require('fs/promises');
+import {watch} from 'node:fs/promises';
 
+(
+    async () =>{
+        const watcher = watch("./commands.txt");
+        for await (const events of watcher){
+            if(events.eventType == 'change' && events.filename == 'commands.txt'){
+                console.log(events);
+            }
+        }
+    }
+)();
