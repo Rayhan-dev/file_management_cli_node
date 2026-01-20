1a5fc919c7263e84f41745211e266971e6e5d916
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-01-20 20:26:18 +0600

    read content from command file

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-import {watch} from 'node:fs/promises';
+import { log } from 'node:console';
+import fs from 'node:fs/promises';
 
 (
     async () =>{
-        const watcher = watch("./commands.txt");
+        const fileHandler = await fs.open('./commands.txt','r');
+        const watcher = fs.watch("./commands.txt");
         for await (const events of watcher){
             if(events.eventType == 'change' && events.filename == 'commands.txt'){
-                console.log(events);
+                const size =  (await fileHandler.stat()).size;
+                const buff = Buffer.alloc(size); //memory optimization 
+                const lenght = size;
+                const position = 0;
+                const offset = 0;
+                const content = await fileHandler.read(buff,offset,lenght,position);
+                console.log(content);
             }
         }
     }
