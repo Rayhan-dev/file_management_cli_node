f477442bf1bd56604cb4e0af901728062b95128b
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-01-21 14:03:49 +0600

    cleaning up code

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ import fs from 'node:fs/promises';
 (
     async () =>{
         const fileHandler = await fs.open('./commands.txt','r');
-        const watcher = fs.watch("./commands.txt");
-        for await (const events of watcher){
-            if(events.eventType == 'change' && events.filename == 'commands.txt'){
-                const size =  (await fileHandler.stat()).size;
+
+        fileHandler.on("change",async ()=>{
+            const size =  (await fileHandler.stat()).size;
                 const buff = Buffer.alloc(size); //memory optimization 
                 const lenght = size;
                 const position = 0;
                 const offset = 0;
-                const content = await fileHandler.read(buff,offset,lenght,position);
-                console.log(content);
+                await fileHandler.read(buff,offset,lenght,position);
+                console.log(buff.toString("utf-8"));
+        })
+        const watcher = fs.watch("./commands.txt");
+        for await (const events of watcher){
+            if(events.eventType == 'change' && events.filename == 'commands.txt'){
+                fileHandler.emit("change");
             }
         }
     }
